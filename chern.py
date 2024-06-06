13ccb79fc5161eb614c56fa9895e070fc4d98621
Sergey <sergeich777@bk.ru>
2024-06-06 17:15:07 +0500

    Init Skillfactory

---
+++ chern.py
@@ -0,0 +1,28 @@
+field = [[" "] * 3 for i in range(3) ]
+
+
+def vvod():
+    while True:
+        cords = input("         Ваш ход: ").split()
+
+        if len(cords) != 2:
+            print(" Введите 2 координаты! ")
+            continue
+
+        x, y = cords
+
+        if not (x.isdigit()) or not (y.isdigit()):
+            print(" Введите числа! ")
+            continue
+
+        x, y = int(x), int(y)
+
+        if 0 > x or x > 2 or 0 > y or y > 2:
+            print(" Координаты вне диапазона! ")
+            continue
+
+        if field[x][y] != " ":
+            print(" Клетка занята! ")
+            continue
+
+        return x, y
